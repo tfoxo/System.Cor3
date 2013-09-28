a8a9e21f271d1def8473c77095001a29986a6d1a
tfwroble <tfwroble@gmail.com>
2013-09-28 06:03:22 -0500

    Added some more includes
    As includes are not added to the build, maybe this isn't the best place
    for them

---
+++ Source/Cor3.Core/Include/ClipboardMonitorService.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+	public class ClipboardMonitorService : IServiceProvider, IDisposable
+	{
+		const string message_handle_zero = "Failed obtaining window handle.";
+
+		// service-container (test)
+		#region Service
+		static readonly ServiceContainer services = new ServiceContainer();
+		object IServiceProvider.GetService(Type serviceType)
+		{
+			return (ClipboardMonitorService) services.GetService(serviceType);
+		}
+		#endregion
+		
+		#region Win32
+		
+		[DllImport(user32)] static extern IntPtr SetClipboardViewer(IntPtr hWnd);
+		[DllImport(user32)] static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+		[DllImport(user32)] static extern int ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNext);
+
+		const string user32 = "user32.dll";
+		const int WM_DRAWCLIPBOARD = 0x308, WM_PAINTCLIPBOARD=0x309, WM_CHANGECBCHAIN = 0x30d;
+		#endregion
+		#region Win32-Hook
+		IntPtr WinProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			switch (msg)
+			{
+				case WM_CHANGECBCHAIN:
+					if (wParam == hWndNextViewer)
+					{
+						// clipboard viewer chain changed, need to fix it.
+						hWndNextViewer = lParam;
+					}
+					else if (hWndNextViewer != IntPtr.Zero)
+					{
+						// pass the message to the next viewer.
+						SendMessage(hWndNextViewer, msg, wParam, lParam);
+					}
+					break;
+				case WM_DRAWCLIPBOARD:
+					// clipboard content changed
+//					lock (LOCK)
+					{
+						OnGotClipboard();
+					}
+					// pass the message to the next viewer.
+					SendMessage(hWndNextViewer, msg, wParam, lParam);
+					break;
+			}
+			return IntPtr.Zero;
+		}
+		#endregion
+		
+		#region Fields
+		Window window;
+		HwndSource hWndSource;
+		IntPtr hWndNextViewer;
+		bool isViewing = false;
+		static readonly object LOCK = new object();
+		#endregion
+		
+		public ClipboardMonitorService(Window window,RoutedEventHandler handler) : this(window)
+		{
+			AddHandler(handler);
+		}
+		public ClipboardMonitorService(Window window)
+		{
+			services.AddService(typeof(ClipboardMonitorService),this,false);
+			this.window = window;
+			Initialize();
+		}
+		
+		private void Initialize()
+		{
+			WindowInteropHelper wih = new WindowInteropHelper(this.window);
+			if (wih.Handle==IntPtr.Zero)
+			{
+				MessageBox.Show(message_handle_zero);
+				return;
+			}
+			hWndSource = HwndSource.FromHwnd(wih.Handle);
+			hWndSource.AddHook(this.WinProc); // start processing window messages
+			hWndNextViewer = SetClipboardViewer(hWndSource.Handle); // set this window as a viewer
+			isViewing = true;
+		}
+		private void Deinitialize()
+		{
+			ChangeClipboardChain(hWndSource.Handle, hWndNextViewer);
+			hWndNextViewer = IntPtr.Zero;
+			hWndSource.RemoveHook(this.WinProc);
+			isViewing = false;
+		}
+
+		#region Event
+		public void AddHandler(RoutedEventHandler handler)
+		{
+			GotClipboard += handler;
+		}
+		public void RemoveHandler(RoutedEventHandler handler)
+		{
+			GotClipboard -= handler;
+		}
+
+		public event RoutedEventHandler GotClipboard;
+		protected virtual void OnGotClipboard()
+		{
+			if (GotClipboard != null) {
+				GotClipboard(window, default(RoutedEventArgs));
+			}
+		}
+		#endregion
+
+		#region IDisposable
+		void IDisposable.Dispose()
+		{
+			services.RemoveService(typeof(ClipboardMonitorService));
+			services.Dispose();
+			Deinitialize();
+		}
+		#endregion
+	}
+}
--- Source/Cor3.Core/System/Commander.cs
+++ Source/Cor3.Core/System/Commander.cs
@@ -1,5 +1,27 @@
-﻿// Copyright (c) t.f.w.
-// this code was put here  for a little wordpress post
+﻿#region User/License
+// Copyright (c) 2005-2013 tfwroble
+// 
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
 // ----------------------------------------------------------------------------
 // 'it just works'
 // ----------------------------------------------------------------------------
@@ -27,9 +49,9 @@ using System.Diagnostics;
 
 namespace System
 {
-  // feel free to optimize and/or 
-  class Commander
- {
+	// feel free to optimize and/or
+	class Commander
+	{
 		static public readonly char[] char_trimExt = {'*','.',' '};
 		static public int ArgIndex = 0;
 		internal List<string> Args, ArgsBackup;
@@ -61,7 +83,7 @@ namespace System
 		
 		/// <summary>
 		/// Get a value and delete from list of arguments: 'Args';
-    /// Something like a Stack.Pop() operation.
+		/// Something like a Stack.Pop() operation.
 		/// </summary>
 		/// <param name="index">parameter index</param>
 		/// <param name="andRemove">default is true.</param>
