771c001c3985890b56097d703285af0e9beabf84
tfoxo <tfwroble@gmail.com>
2013-11-16 09:23:18 -0600

    add NormalizePathTask/Tool

---
+++ Source-Console/NormalizePathTask/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oio
+ * Date: 11/16/2013
+ * Time: 9:19 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Tasks;
+
+namespace NormalizePathTool
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+            if (args.Length != 2)
+            {
+            	Console.WriteLine("Nothing to do");
+            	Console.ReadKey(true);
+            	return;
+            }
+            
+            NormalizePathTask task = new NormalizePathTask(args[0],args[1]);
+            	Console.ReadKey(true);
+            	return;
+		}
+	}
+}
