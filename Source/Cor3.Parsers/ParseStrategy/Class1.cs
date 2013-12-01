c8e5d48344a36d9475f1a8daac67bd8baa9aaa52
tfoxo <tfwroble@gmail.com>
2013-12-01 07:50:40 -0600

    Adding System.Cor3.Parsers

---
+++ Source/Cor3.Parsers/ParseStrategy/Class1.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oio
+ * Date: 12/1/2013
+ * Time: 7:26 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace System.Cor3.Parsers.ParseStrategy
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Class1
+	{
+		public Class1()
+		{
+		}
+	}
+}
