8f04480c51e641cc749e3a44bd7cc6c0e53c6979
tfoxo <tfwroble@gmail.com>
2014-03-21 01:37:28 -0500

    Images + Added / Moved refactory

---
+++ Source-Generator/GenaratorLib/Systems/Parser/GeneratorParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Generator.Core.Parser
+{
+	public class GeneratorParser : System.ComponentModel.BackgroundWorker
+	{
+		public string ParserResult { get;set; }
+		public IDbConfiguration4 Configuration { get;set; }
+		public Action<string> ResultAction { get;set; }
+		
+		public void Prepare(IDbConfiguration4 configuration)
+		{
+			Configuration = configuration;
+		}
+		
+		protected override void OnDoWork(System.ComponentModel.DoWorkEventArgs e)
+		{
+			base.OnDoWork(e);
+			ParserResult = TemplateFactory.ConvertTable(Configuration);
+		}
+		
+		protected override void OnRunWorkerCompleted(System.ComponentModel.RunWorkerCompletedEventArgs e)
+		{
+			base.OnRunWorkerCompleted(e);
+			if (ResultAction!=null) ResultAction.Invoke(ParserResult);
+			this.Configuration = null;
+			this.ParserResult = null;
+			GC.Collect();
+		}
+		
+		protected override void OnProgressChanged(System.ComponentModel.ProgressChangedEventArgs e)
+		{
+			base.OnProgressChanged(e);
+		}
+		
+	}
+}
